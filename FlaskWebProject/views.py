1574b6c7404ed4a442d1ed755287add5b6eb7ba4
danschler <44245148+danschler@users.noreply.github.com>
2026-03-17 15:36:17 +0100

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -71,7 +71,7 @@ def login():
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         if form.username.data == "admin":
-            app.logger.info("Admin logged in successfully")
+            app.logger.warning("Admin logged in successfully")
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
